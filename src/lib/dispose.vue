a3b84d017c8c49b27ac79c1c982b6457c0699dda
handsomeBoy <>
2019-07-16 23:29:23 +0800

    add area data and processing area data

---
+++ src/lib/dispose.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>hello </h1>
+</template>
+<script>
+import Data from './data'; //处理的数据
+export default {
+  data(){
+    return {
+      province:[],//省
+      city:[],//市
+      area:[],//区
+    }
+  },
+  created(){
+    //过滤省市区的数据
+  }
+}
+</script>
