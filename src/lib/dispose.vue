0721d31cd19e3ea448b4a88f2a2cc6e1d6009e7b
handsomeBoy <>
2019-07-18 09:07:00 +0800

    数据格式化

--- src/lib/dispose.vue
+++ src/lib/dispose.vue
@@ -6,22 +6,41 @@ import Data from './data'; //处理的数据
 export default {
   data(){
     return {
-      province:[],//省
+      province:{},//省
       city:[],//市
       area:[],//区
+      acount:[]
     }
   },
   created(){
-    //过滤省市区的数据
-    Data.forEach((region, index) => { //获取省的数据
-      if(region.code.slice(-4) == '0000'){
-        this.province.push(region);
-      }else if(region.code.slice(-4) != '0000' && region.code.slice(-2) == '00'){ //获取市的数据
-        this.city.push(region);
-      }else{ //获取县的数据
-         this.area.push(region);
+    this.province = Data.province;
+    this.city = Data.city;
+    this.area = Data.area;
+
+    for(let i = 0; i < this.province.length; i++){
+      let province = {
+        province: this.province[i]
+      };
+      let provinceCode = this.province[i].code.substr(0,2); //获取头两位
+      let city = [];
+      for(let ii = 0; ii < this.city.length; ii++){ //获取省下的市
+        if(this.city[ii].code.substr(0,2) == provinceCode){
+
+          city.push(this.city[ii]);
+        }
       }
-    })
+      if(city.length == 0){
+        let code = this.province[i].code;
+        province.city = [{
+          name:this.province[i].name,
+          code: this.province[i].code.replace(/.{3}$/,'100'), //转换最后三位
+        }]
+      }else{
+        province.city = city;
+      }
+      this.acount.push(province);
+    }
+    console.log(this.acount);
     
   }
 }
